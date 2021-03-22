ab3a0966f023ddc27ce6822d9434500d9b09d9a2
hemery <fred.hemery@univ-artois.fr>
2021-03-22 18:52:45 +0100

    :arrow_up: ajout ngx-moment en français

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MessagesModule} from 'primeng/messages';
+import {ToastModule} from 'primeng/toast';
+import {MessageService} from 'primeng/api';
+import {registerLocaleData} from '@angular/common';
+import {MomentModule} from 'ngx-moment';
+import 'moment/locale/fr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
@@ -10,9 +19,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MomentModule,
+    MessagesModule,
+    ToastModule,
+
   ],
-  providers: [],
+  providers: [MessageService, {provide: LOCALE_ID, useValue: 'fr-FR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
