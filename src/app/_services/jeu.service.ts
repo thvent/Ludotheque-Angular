1a2ec7ec99a1c6820cf7418c0690a7a6ea384727
Barbier Louis <louis_barbier@ens.univ-artois.fr>
2021-03-30 11:02:28 +0200

    jeu.service.ts

---
+++ src/app/_services/jeu.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {Observable, of, throwError} from 'rxjs';
+import {Jeu} from '../_models/jeu';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {catchError, map} from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JeuService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getProfile(): Observable<Jeu> {
+    return this.http.get<any>(environment.apiUrl + '/auth/jeux', httpOptions)
+      .pipe(
+        map(rep => rep.data.item),
+        catchError(err => throwError(err))
+      );
+  }
+}
---
+++ src/app/details-jeu/details-jeu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-jeu',
+  templateUrl: './details-jeu.component.html',
+  styleUrls: ['./details-jeu.component.css']
+})
+export class DetailsJeuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
