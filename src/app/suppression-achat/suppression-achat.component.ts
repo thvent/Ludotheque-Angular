156a3dbdd01912de253d2dd0518123dd058480f6
thibaut.herbet <thibaut.herbet@info0411.iut-lens.univ-artois.fr>
2021-03-31 15:52:04 +0200

    issue_27

--- src/app/suppression-achat/suppression-achat.component.ts
+++ src/app/suppression-achat/suppression-achat.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../_services/user.service';
+import {AuthentificationService} from '../_services/authentification.service';
+import {filter, map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {Jeu} from '../_models/jeu';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-suppression-achat',
@@ -8,14 +13,32 @@ import {UserService} from '../_services/user.service';
 })
 export class SuppressionAchatComponent implements OnInit {
 
-  constructor(private userService:UserService) { }
+  jeuxAchetes:Observable<Jeu>;
+
+  form: any = {
+    jeu_id: null
+  };
+
+  formulaire = new FormGroup({
+    jeu_id: new FormControl('', Validators.required),
+  });
+
+  constructor(private authService: AuthentificationService, private userService: UserService) { }
 
   ngOnInit(): void {
-    this.userService.getDetailedUser();
+    this.userService.getDetailedUser(this.authService.userValue.id).pipe(
+      map(user => user.jeuxAchetes)
+    ).subscribe(
+      rep => this.jeuxAchetes = rep;
+    );
   }
 
   onSubmit(): void {
-
+    this.form = {...this.form, ...this.formulaire.value};
+    let jeu:Jeu;
+    this.jeuxAchetes.subscribe()
+    this.userService.suppression_achat(
+    ));
   }
 
 }
