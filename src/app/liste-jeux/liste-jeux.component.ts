85e6366d432db3c0d4d5007b42bb459d9ac17ba2
antoine_pagnier <antoine_pagnier@ens.univ-artois.fr>
2021-03-31 13:42:52 +0100

    filtrage des jeux par nombres de joueur

--- src/app/liste-jeux/liste-jeux.component.ts
+++ src/app/liste-jeux/liste-jeux.component.ts
@@ -4,6 +4,7 @@ import {Jeu} from '../_models/jeu';
 import {JeuService} from '../_services/jeu.service';
 import {MessageService} from 'primeng/api';
 import {noop, Observable, of} from 'rxjs';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-liste-jeux',
@@ -12,6 +13,20 @@ import {noop, Observable, of} from 'rxjs';
 })
 export class ListeJeuxComponent implements OnInit {
   jeux$: Observable<Jeu>;
+  jeuxFiltre$: Observable<Jeu>;
+  nbrJoueur: number;
+
+  formulaire = new FormGroup({
+    nombreJoueurs: new FormControl('', Validators.required),
+  });
+
+  form: any = {
+    nombreJoueurs: null,
+  };
+
+  loading = false;
+  returnUrl: string;
+  error = '';
 
   constructor(private router: Router, private messageService: MessageService, private route: ActivatedRoute, private service: JeuService) {
   }
@@ -24,14 +39,20 @@ export class ListeJeuxComponent implements OnInit {
     console.log(lesJeux);
   }
 
-  ok(): void {
-    const lesJeuxFiltre = [];
-    this.service.getJeux().subscribe(
-      str => lesJeuxFiltre.push(str.nombre_joueurs = 2), noop, () => this.jeux$ = of(lesJeuxFiltre[0])
-    );
-    console.log(lesJeuxFiltre);
+  getNbrJoueurs(): number{
+    return this.form.nombreJoueurs;
+  }
+  onSubmit(): void {
+    this.form = {
+      ...this.form, ...this.formulaire.value,
+    };
+    this.nbrJoueur = this.form.nombreJoueurs;
   }
 
+  showDiv(): void {
+    document.getElementById('filtre').style.display = 'block';
+    console.log('ok');
+  }
 
   jeuSelectionne(jeu: Jeu): void {
     this.router.navigate(['/jeu', jeu.id]);
